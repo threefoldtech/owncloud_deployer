af484bb9b117ee2ed36f1c6589e8ca02ef1ba053
samaradel <samar@incubaid.com>
2022-02-01 12:39:14 +0200

    Add requests

--- jumpscale/packages/owncloud/frontend/src/views/Requests.vue
+++ jumpscale/packages/owncloud/frontend/src/views/Requests.vue
@@ -5,7 +5,14 @@
       :headers="headers"
       :items="requests"
       :items-per-page="5"
-    ></v-data-table>
+      loading
+      loading-text="Loading... Please wait"
+    >
+      <template v-slot:item.email="{ item }">{{
+        item.email == "" ? "-" : item.email
+      }}</template>
+      <template v-slot:item.time="{ item }">{{ timeSince(item) }}</template>
+    </v-data-table>
     <div class="text-center pt-2 mt-10">
       <v-btn color="primary" class="mr-2"> btn 1 </v-btn>
       <v-btn color="primary"> btn 2 </v-btn>
@@ -32,11 +39,37 @@ export default {
     getRequests() {
       Service.getRequests()
         .then((response) => {
-          console.log(response);
+          this.requests = response.data;
         })
         .catch((error) => {
           console.log("Error! Could not reach the API. " + error);
         });
+    },
+    timeSince(date) {
+      var seconds = Math.floor((new Date() - date) / 1000);
+
+      var interval = seconds / 31536000;
+
+      if (interval > 1) {
+        return Math.floor(interval) + " years";
+      }
+      interval = seconds / 2592000;
+      if (interval > 1) {
+        return Math.floor(interval) + " months";
+      }
+      interval = seconds / 86400;
+      if (interval > 1) {
+        return Math.floor(interval) + " days";
+      }
+      interval = seconds / 3600;
+      if (interval > 1) {
+        return Math.floor(interval) + " hours";
+      }
+      interval = seconds / 60;
+      if (interval > 1) {
+        return Math.floor(interval) + " minutes";
+      }
+      return Math.floor(seconds) + " seconds";
     },
   },
   mounted() {
