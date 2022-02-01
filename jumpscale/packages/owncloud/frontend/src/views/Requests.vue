a9a808ae73ec1acfbc334b6aeda0eda93fa30b60
samaradel <samar@incubaid.com>
2022-02-01 16:26:31 +0200

    Add export fun

--- jumpscale/packages/owncloud/frontend/src/views/Requests.vue
+++ jumpscale/packages/owncloud/frontend/src/views/Requests.vue
@@ -73,12 +73,62 @@ export default {
     },
     exportData() {
       Service.exportData()
-        .then(() => {
-          console.log("Data exported!");
+        .then((response) => {
+          var json = $.parseJSON(response.data);
+          var csv = this.JSON2CSV(json);
+          var downloadLink = document.createElement("a");
+          var blob = new Blob(["\ufeff", csv]);
+          var url = URL.createObjectURL(blob);
+          downloadLink.href = url;
+          downloadLink.download = "data.csv";
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
         })
         .catch((error) => {
           console.log("Error! Could not reach the API. " + error);
         });
+    },
+    JSON2CSV(objArray) {
+      var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
+      var str = "";
+      var line = "";
+
+      if ($("#labels").is(":checked")) {
+        var head = array[0];
+        if ($("#quote").is(":checked")) {
+          for (var index in array[0]) {
+            var value = index + "";
+            line += '"' + value.replace(/"/g, '""') + '",';
+          }
+        } else {
+          for (var index in array[0]) {
+            line += index + ",";
+          }
+        }
+
+        line = line.slice(0, -1);
+        str += line + "\r\n";
+      }
+
+      for (var i = 0; i < array.length; i++) {
+        var line = "";
+
+        if ($("#quote").is(":checked")) {
+          for (var index in array[i]) {
+            var value = array[i][index] + "";
+            line += '"' + value.replace(/"/g, '""') + '",';
+          }
+        } else {
+          for (var index in array[i]) {
+            line += array[i][index] + ",";
+          }
+        }
+
+        line = line.slice(0, -1);
+        str += line + "\r\n";
+      }
+      return str;
     },
     time(ts) {
       var timestamp = moment.unix(ts);
