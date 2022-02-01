03be0cc37696b2a8b48f856ac13c13c0afe58164
Waleed <waleed.hammam@gmail.com>
2022-02-01 11:31:32 +0200

    use hash router

--- jumpscale/packages/owncloud/frontend/src/router/index.js
+++ jumpscale/packages/owncloud/frontend/src/router/index.js
@@ -25,3 +25,13 @@ const router = new VueRouter({
 });
 
 export default router;
+
+router.beforeEach((to, from, next) => {
+  // Redirect if fullPath begins with a hash (ignore hashes later in path)
+  if (to.fullPath.substring(0, 2) === "/#") {
+    const path = to.fullPath.substring(2);
+    next(path);
+    return;
+  }
+  next();
+});
