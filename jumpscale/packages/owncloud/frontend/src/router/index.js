45f267a0f299ccd5e83a53c48f815ac92809bf8a
Waleed <waleed.hammam@gmail.com>
2022-02-02 15:30:45 +0200

    fix hash router

--- jumpscale/packages/owncloud/frontend/src/router/index.js
+++ jumpscale/packages/owncloud/frontend/src/router/index.js
@@ -19,19 +19,9 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  mode: "history",
   base: process.env.BASE_URL,
   routes,
 });
 
 export default router;
 
-router.beforeEach((to, from, next) => {
-  // Redirect if fullPath begins with a hash (ignore hashes later in path)
-  if (to.fullPath.substring(0, 2) === "/#") {
-    const path = to.fullPath.substring(2);
-    next(path);
-    return;
-  }
-  next();
-});
