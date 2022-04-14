136236dcf17b79c679014f083cb92644ccd80a44
Sameh-farouk <samehabouelsaad@gmail.com>
2022-04-14 20:05:54 +0200

    use poetry run instead of poetry shell

--- docker/init.sh
+++ docker/init.sh
@@ -18,11 +18,11 @@ mkdir -p /data/jsngmodel && ln -s /data/jsngmodel ~/.config/jumpscale
 sed -i "s/domain = \"waleed.threefold.io\"/domain = \"$domain\"/g" /owncloud_deployer/jumpscale/packages/owncloud/package.toml 
 
 # Create a dummy identity to start jsng with Threefold Connect
-jsng "ident=j.core.identity.new(\"default\", \"$dummy_tname\", \"$dummy_email\", network=\"$explorer_network\", admins=$ADMINS); ident.register(); ident.save()"
+poetry run jsng "ident=j.core.identity.new(\"default\", \"$dummy_tname\", \"$dummy_email\", network=\"$explorer_network\", admins=$ADMINS); ident.register(); ident.save()"
 
 # Create the default threebot
-jsng 'j.servers.threebot.new("default"); j.servers.threebot.default.save()'
-jsng "j.servers.threebot.default.packages.add(path='/owncloud_deployer/jumpscale/packages/owncloud')"
+poetry run jsng 'j.servers.threebot.new("default"); j.servers.threebot.default.save()'
+poetry run jsng "j.servers.threebot.default.packages.add(path='/owncloud_deployer/jumpscale/packages/owncloud')"
 
 # Set email server config
-jsng "email_server_config = {\"host\": \"$email_host\", \"port\": "$email_port", \"username\": \"$email_username\", \"password\": \"$email_password\"}; j.core.config.set(\"EMAIL_SERVER_CONFIG\", email_server_config)"
+poetry run jsng "email_server_config = {\"host\": \"$email_host\", \"port\": "$email_port", \"username\": \"$email_username\", \"password\": \"$email_password\"}; j.core.config.set(\"EMAIL_SERVER_CONFIG\", email_server_config)"
