e467777e8e735eff095054346b2fa169c90459a8
Sameh-farouk <samehabouelsaad@gmail.com>
2022-04-17 18:11:24 +0200

    fix init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -19,7 +19,7 @@ mkdir -p /data/jsngmodel && ln -s /data/jsngmodel ~/.config/jumpscale
 sed -i "s/domain = \"waleed.threefold.io\"/domain = \"$domain\"/g" /owncloud_deployer/jumpscale/packages/owncloud/package.toml 
 
 # Edit the default support ssh key with the passed one
-[[ -z "${SUPPORT_PUBLIC_SSH_KEY}" ]] && sed -i "s/SSH_KEY     = \"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSkhNAIP22RH/sQx7alFS6vcqw1OAQUkC5MLv6t3L78YVTRv+/owSqIqCQHr2+zfb3aJijsxj9nqg54rVkEiCXOkT6IE/MGWSP6O/x/cCG8J7AT+OCCjo9IB/+V3CA8yREHi7ggqPv6hEfNoa1AMbnxqxT7a+5sJUVd14/Ib9OQKWBCXzosa0SjTY/RO1SrL93E80N+SJQRBCMemzlepn4wLDWvqs7DiruY+g9E2CskhDijt4iJCuNFZzAcTS3UeqxOG2QfLK2zc8M9/AycMcEyHn94Lml6V75Lk09iLB9QGTGsa4oAD3GFLce4VoKKZx0e6lwwnMNoAHKhBEMFmO5 root@waleed-ng\"/SSH_KEY     = \"$SUPPORT_PUBLIC_SSH_KEY\"/g" /root/tf_source_module/main.tf
+[[ ! -z "${SUPPORT_PUBLIC_SSH_KEY}" ]] && sed -i "s:\"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDSkhNAIP22RH/sQx7alFS6vcqw1OAQUkC5MLv6t3L78YVTRv+/owSqIqCQHr2+zfb3aJijsxj9nqg54rVkEiCXOkT6IE/MGWSP6O/x/cCG8J7AT+OCCjo9IB/+V3CA8yREHi7ggqPv6hEfNoa1AMbnxqxT7a+5sJUVd14/Ib9OQKWBCXzosa0SjTY/RO1SrL93E80N+SJQRBCMemzlepn4wLDWvqs7DiruY+g9E2CskhDijt4iJCuNFZzAcTS3UeqxOG2QfLK2zc8M9/AycMcEyHn94Lml6V75Lk09iLB9QGTGsa4oAD3GFLce4VoKKZx0e6lwwnMNoAHKhBEMFmO5 root@waleed-ng\":\"$SUPPORT_PUBLIC_SSH_KEY\":g" /root/tf_source_module/main.tf
 
 # Create a dummy identity to start jsng with Threefold Connect
 jsng "ident=j.core.identity.new(\"default\", \"$dummy_tname\", \"$dummy_email\", network=\"$explorer_network\", admins=$ADMINS); ident.register(); ident.save()"
