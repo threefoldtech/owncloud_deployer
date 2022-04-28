d74c3ebba6f94949db728bce669d96bde3f618c0
Sameh-farouk <samehabouelsaad@gmail.com>
2022-04-28 11:42:48 +0200

    using package install method to set the package domain and letsencrypt email

---
+++ jumpscale/packages/owncloud/package.py
@@ -0,0 +1,25 @@
+import toml
+
+PACKAGE_CONFIG_PATH = 'package.toml'
+
+class owncloud:
+    def install(self, **kwargs):
+        domain = kwargs.get('domain')
+        letsencryptemail = kwargs.get('letsencryptemail')
+        if not all([domain, letsencryptemail]):
+            raise(f'usage: j.servers.threebot.default.packages.add(path="<package_path>", domain="<domain>", letsencryptemail="<letsencryptemail>")')
+        
+        with open(PACKAGE_CONFIG_PATH, 'rt') as f:
+            parsed_toml = toml.load(f)
+        
+        if domain:
+            parsed_toml['servers'][0]['domain'] = domain
+            print(f"set the domain to {domain}")
+
+        if letsencryptemail:
+            parsed_toml['servers'][0]['letsencryptemail'] = letsencryptemail
+            print(f"set the email to {letsencryptemail}")
+
+        with open(PACKAGE_CONFIG_PATH, 'wt') as f:
+            _ = toml.dump(parsed_toml, f)
+        print('Package installation complete!')
